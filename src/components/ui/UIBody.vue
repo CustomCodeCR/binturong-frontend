37eab5f98269b871fa696b70ba0b5f2956e809f7
MauLang18 <maulangbonilla.18@gmail.com>
2026-01-26 13:22:42 -0600

    update

---
+++ src/components/ui/UIBody.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import type { HTMLAttributes, PropType } from "vue";
+
+type sizes = "1xl" | "l" | "m" | "s";
+
+const props = defineProps({
+  element: {
+    type: String as PropType<string>,
+    default: "p",
+  },
+  size: {
+    type: String as PropType<sizes>,
+    default: "m",
+  },
+  body_atts: {
+    type: Object as PropType<HTMLAttributes>,
+    default: {},
+  },
+});
+
+// Map body size to corresponding class
+const getBodyClass = () => {
+  return [`vx-text-body-${props.size}`];
+};
+</script>
+
+<template>
+  <component
+    :is="props.element"
+    v-bind="props.body_atts"
+    class="max-w-[50rem]"
+    :class="getBodyClass()"
+  >
+    <slot></slot>
+  </component>
+</template>
---
+++ src/components/ui/UIHeading.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import type { HTMLAttributes, PropType } from "vue";
+
+type headings = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+type sizes = "1" | "2" | "3" | "4" | "5" | "6";
+
+const props = defineProps({
+  element: {
+    type: String as PropType<headings>,
+    default: "h3",
+  },
+  size: {
+    type: String as PropType<sizes>,
+    default: "3",
+  },
+  heading_atts: {
+    type: Object as PropType<HTMLAttributes>,
+    default: {},
+  },
+});
+
+// Map heading element to corresponding class
+const getHeadingClass = () => {
+  return [`vx-text-heading-${props.size}`];
+};
+</script>
+
+<template>
+  <component
+    :is="props.element"
+    v-bind="props.heading_atts"
+    class="max-w-[50rem]"
+    :class="getHeadingClass()"
+  >
+    <slot></slot>
+  </component>
+</template>

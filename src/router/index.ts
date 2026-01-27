d26ceb1ab0751e9cd9616a009211bad1b885d148
MauLang18 <maulangbonilla.18@gmail.com>
2026-01-26 18:35:52 -0600

    login add

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Login from "@/components/Login.vue";
 
 export const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: "/",
+      path: "/home",
       name: "home",
       component: HelloWorld,
+    },
+    {
+      path: "/",
+      name: "login",
+      component: Login,
     },
   ],
 });
